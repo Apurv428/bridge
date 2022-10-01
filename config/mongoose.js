e35885f32e8289ea81d0c02111b07061615c5b41
Pratiksha Sankhe <pssankhe_b20@ce.vjti.ac.in>
2022-10-01 17:02:36 +0530

    Files

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect('mongodb://localhost:27017/project');
+
+db=mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
+
